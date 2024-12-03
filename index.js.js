44d9ebfc4832b3e9912716b2b62d7e3338d4e05c
tunguyen10112006 <nguyenviettuantu82@gmail.com>
2024-12-03 19:54:07 +0700

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,8 @@
+// hiển thị một dòng văn bản
+document.write("hello ")
+//hiển thị hộp thoại thông báo 
+alert("Chào mừng  các bạn đến với ÔNG TRÙ VẤP CỎ !\n Bấm ok để XEM ");
+// nhạp giữ liệu
+var name = prompt("Nhập tên của bạn")
+document.write("chào bạn "+ name +" . thỰC ĂN VẠ !");
+var question = confirm("Bạn thực sự muốn truy cập vào trang  này ?")
